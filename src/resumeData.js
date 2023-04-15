5e2e4ed1ce35863b132192936a8abac0faf82d3c
chicuongdx <chicuongdx2001@gmail.com>
2023-04-15 15:06:04 +0700

    init project

---
+++ src/resumeData.js
@@ -0,0 +1,90 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Cuong Nguyen",
+    "role": "Machine Learning Engineer",
+    "linkedinId":"https://www.linkedin.com/in/chi-nguyen-270486233/",
+    "skypeid": "",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/chi-nguyen-270486233/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://www.linkedin.com/in/chi-nguyen-270486233/",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I'm currently a student at University of Information Technology, VNU-HCM. I'm a Machine Learning Engineer who is passionate about making open-source more accessible, creating technology to elevate people, and building community. Some technologies I enjoy working with include Python, Tensorflow, Keras, Pytorch, OpenCV, and React.",
+    "address":"Vietnam",
+    "website":"https://www.facebook.com/icloudx.130",
+    "education":[
+      {
+        "UniversityName":"University of Information Technology, VNU-HCM",
+        "specialization":"Computer Sicence",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2023",
+        "Achievements":"Bachelor"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"EyeQ Tech",
+        "specialization":"Software Engineer Internship",
+        "MonthOfLeaving":"Mar",
+        "YearOfLeaving":"2023",
+        "Achievements":"Completed some project using .NET, ReactJS"
+      }
+    ],
+    "skillsDescription":"Have experience with some technologies below",
+    "skills":[
+      {
+        "skillname":".NET"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"NestJS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
